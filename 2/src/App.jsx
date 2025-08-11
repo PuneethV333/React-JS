afca3d0b7da6d01deeca91a0c91cfd5e39c44d0f
PuneethV333 <puneethvijayakumar333@gmail.com>
2025-08-11 20:56:10 +0530

    new

---
+++ 2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='border-x-orange-300'>
+      <h1 className='uppercase bg-amber-200 p-1.5' >hello world</h1>
+    </div>
+  )
+}
+
+export default App
